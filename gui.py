6eb8a9084283361cc7928a458ffa7c27da46e36f
samytichadou <samytichadou@gmail.com>
2019-05-21 08:24:14 +0200

    get gui emboss back

--- gui.py
+++ gui.py
@@ -13,9 +13,9 @@ class MenuPanel(bpy.types.Panel):
         layout = self.layout
         row = layout.row(align=True)
         row.operator('reload.reload_all', text='',
-                     icon='FILE_REFRESH', emboss=False)
+                     icon='FILE_REFRESH')
         if bpy.data.window_managers['WinMan'].reload_modal == True:
             row.prop(bpy.data.window_managers['WinMan'],
                      'reload_modal', text='', icon='CANCEL')
         else:
-            row.operator('reload.reload_timer', text='', icon='TIME', emboss=False)
+            row.operator('reload.reload_timer', text='', icon='TIME')
