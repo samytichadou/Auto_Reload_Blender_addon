a6de54a7d77609f86a6807c12414ed0a48d86fcb
samytichadou <samytichadou@gmail.com>
2019-06-27 11:57:23 +0200

    idea for preview

---
+++ dev/misc.py
@@ -0,0 +1,6 @@
+#ui preview 
+#from simple asset manager
+row.template_icon_view(wm, "asset_manager_prevs", show_labels=True)
+
+#link to api
+https://docs.blender.org/api/master/bpy.types.UILayout.html?highlight=template_icon_view#bpy.types.UILayout.template_icon_view
